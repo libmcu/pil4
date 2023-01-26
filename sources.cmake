2075309606a5c034f7df33a0f97c42e32c56dbcd
Kyunghwan Kwon <k@mononn.com>
2023-01-26 16:34:49 +0900

    Fix CMake working directory

--- sources.cmake
+++ sources.cmake
@@ -1,10 +1,10 @@
 # SPDX-License-Identifier: MIT
 
 if(TARGET_PLATFORM STREQUAL esp32)
-	list(APPEND PL4_SRCS ${CMAKE_CURRENT_SOURCE_DIR}/src/esp32_tls.c)
+	list(APPEND PL4_SRCS ${CMAKE_CURRENT_LIST_DIR}/src/esp32_tls.c)
 else()
 	message(WARN "No target specified. Building with dummy target")
-	list(APPEND PL4_SRCS ${CMAKE_CURRENT_SOURCE_DIR}/src/dummy.c)
+	list(APPEND PL4_SRCS ${CMAKE_CURRENT_LIST_DIR}/src/dummy.c)
 endif()
 
-list(APPEND PL4_INCS ${CMAKE_CURRENT_SOURCE_DIR}/include)
+list(APPEND PL4_INCS ${CMAKE_CURRENT_LIST_DIR}/include)
